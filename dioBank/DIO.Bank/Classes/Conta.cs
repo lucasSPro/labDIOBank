a23603d3590428d06ceeb0f064d220e6945c9ff5
Lucas dos Santos <31446385+lucasSPro@users.noreply.github.com>
2021-01-20 00:02:28 -0300

    add class Conta

---
+++ dioBank/DIO.Bank/Classes/Conta.cs
@@ -0,0 +1,10 @@
+namespace DIO.Bank
+{
+    public class Conta
+    {
+        private TipoConta TipoConta{get; set;}
+        private double  Saldo {get; set;}
+        private double Credito {get; set;}
+        private string Nome {get; set;}
+    }
+}
