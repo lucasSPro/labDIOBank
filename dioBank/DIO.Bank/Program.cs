3ff2589b6ad29debda1b61a7539a09d53eca03bf
Lucas dos Santos <31446385+lucasSPro@users.noreply.github.com>
2021-01-20 14:02:53 -0300

    add opcaoUsuario

--- dioBank/DIO.Bank/Program.cs
+++ dioBank/DIO.Bank/Program.cs
@@ -9,5 +9,22 @@ namespace DIO.Bank
             Conta minhaConta = new Conta(TipoDeConta.PessoaFisica, 0, 0, "Lucas Santos");
             Console.WriteLine(minhaConta.ToString());
         }
+        private static string ObterOpcaoUsiario()
+        {
+            Console.WriteLine();
+            Console.WriteLine(" DIO Bank a seu dispor ");
+            Console.WriteLine("Informe a opção desejada: ");
+            Console.WriteLine("1 - Listar contas");
+            Console.WriteLine("2 - Inserir nova conta");
+            Console.WriteLine("3 - Transferr");
+            Console.WriteLine("4 - Sacar");
+            Console.WriteLine("5 - Depositar");
+            Console.WriteLine("C - Limpar tela");
+            Console.WriteLine("X - Sair");
+
+            string opcaoUsuario =  Console.ReadLine().ToUpper();
+            Console.WriteLine();
+            return opcaoUsuario;
+        }
     }
 }
